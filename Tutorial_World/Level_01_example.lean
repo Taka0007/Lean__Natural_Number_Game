c273fe731bae9832197064c437c7de4f161557d3
Taka <103984069+Taka0007@users.noreply.github.com>
2026-02-15 02:22:44 +0900

    add comment

--- Tutorial_World/Level_01_example.lean
+++ Tutorial_World/Level_01_example.lean
@@ -2,14 +2,14 @@ import Mathlib.Tactic
 import Mathlib.Util.Delaborators
 open Nat
 
-
+-- 今回扱った問題
 example (x q : ℕ) : 37 * x + q = 37 * x + q := by
 rfl
 
-
+-- 2X + 0 = 2X
 example (x : ℕ) : 2*x + 0 = 2*x := by
   rfl
 
-
+-- 2+2 = 4
 example : 2+2 = 4 := by
   rfl
