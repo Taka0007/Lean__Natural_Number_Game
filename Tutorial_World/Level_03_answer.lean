284c2df02586fe6a9972c9cf7cd29506f6a0ac70
Taka <103984069+Taka0007@users.noreply.github.com>
2026-02-15 21:42:48 +0900

    Create Level_03_answer.lean

---
+++ Tutorial_World/Level_03_answer.lean
@@ -0,0 +1,12 @@
+-- Date: 
+-- Version: Latest Mathlib
+
+import Mathlib.Tactic
+open Nat
+
+-- 実はrflだけで証明可能
+-- NNGではtwo_eq_succ_oneが定義されているが、今回のコードでは定義されていないのでNNGとまったく同じようにするためには定理を定義しなくちゃいけない
+example : 2 = succ (succ 0) := by
+-- rw [two_eq_succ_one]
+-- rw [one_eq_succ_zero]
+rfl
