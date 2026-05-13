3078e4f226b76b464853689be0f8fc5a02c6ab89
Taka <103984069+Taka0007@users.noreply.github.com>
2026-05-13 15:51:27 +0900

    Update Level_07_answer.lean

--- Tutorial_World/Level_07_answer.lean
+++ Tutorial_World/Level_07_answer.lean
@@ -5,7 +5,7 @@ import Mathlib.Tactic
 open Nat
 
 theorem succ_eq_add_one n : succ n = n + 1 := by
-rw [one_eq_succ_zero]
-rw [add_succ n 0]
-rw [add_zero]
+-- rw [one_eq_succ_zero]  ##  succ n = n + succ 0
+-- rw [add_succ n 0]      ##  succ n = succ (n + 0)
+-- rw [add_zero]          ##  succ n = succ n
 rfl
