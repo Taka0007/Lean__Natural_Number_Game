5c364b3fd547230e8a72d8a5041d95c72367d627
Taka <103984069+Taka0007@users.noreply.github.com>
2026-05-13 15:21:35 +0900

    Update Level_05_answer.lean

--- Tutorial_World/Level_05_answer.lean
+++ Tutorial_World/Level_05_answer.lean
@@ -5,4 +5,6 @@ import Mathlib.Tactic
 open Nat
 
 example (a b c : ℕ) : a + (b + 0) + (c + 0) = a + b + c := by
+-- rw [add_zero]   ##  a + b + (c + 0) = a + b + c
+-- rw [add_zero]   ##  a + b + c = a + b + c
 rfl
