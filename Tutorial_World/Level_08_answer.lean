1ca053f8896f5a209fcd76e731a4f5da35055e02
Taka <103984069+Taka0007@users.noreply.github.com>
2026-06-10 16:18:24 +0900

    Update Level_08_answer.lean

--- Tutorial_World/Level_08_answer.lean
+++ Tutorial_World/Level_08_answer.lean
@@ -5,13 +5,13 @@ import Mathlib.Tactic
 open Nat
 
 example : 2 + 2 = 4 := by
--- rw [four_eq_succ_three]
--- rw [succ_eq_add_one]
--- rw [three_eq_succ_two]
--- rw [succ_eq_add_one]
--- nth_rewrite 2 [one_eq_succ_zero]
--- nth_rewrite 2 [two_eq_succ_one]
--- rw [add_succ]
--- rw [add_succ]
--- rw [add_zero]
+-- rw [four_eq_succ_three]  ## 2 + 2 = succ 3
+-- rw [succ_eq_add_one]     ## 2 + 2 = 3 + 1
+-- rw [three_eq_succ_two]   ## 2 + 2 = succ 2 + 1
+-- rw [succ_eq_add_one]     ## 2 + 2 = 2 + 1 + 1
+-- nth_rewrite 2 [one_eq_succ_zero]  ## 2 + 2 = 2 + 1 + succ 0
+-- nth_rewrite 2 [two_eq_succ_one]   ## 2 + succ 1 = 2 + 1 + succ 0
+-- rw [add_succ]                     ## succ (2 + 1) = 2 + 1 + succ 0
+-- rw [add_succ]                     ## succ (2 + 1) = succ (2 + 1 + 0)
+-- rw [add_zero]                     ## succ (2 + 1) = succ (2 + 1)
   rfl
