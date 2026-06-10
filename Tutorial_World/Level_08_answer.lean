9c1fa19842943841adb4bc067ea4400b78c2fd8f
Taka <103984069+Taka0007@users.noreply.github.com>
2026-06-10 16:16:17 +0900

    add answer

--- Tutorial_World/Level_08_answer.lean
+++ Tutorial_World/Level_08_answer.lean
@@ -1,1 +1,17 @@
+-- Date: 2026/06/10
+-- Version: Latest Mathlib
 
+import Mathlib.Tactic
+open Nat
+
+example : 2 + 2 = 4 := by
+-- rw [four_eq_succ_three]
+-- rw [succ_eq_add_one]
+-- rw [three_eq_succ_two]
+-- rw [succ_eq_add_one]
+-- nth_rewrite 2 [one_eq_succ_zero]
+-- nth_rewrite 2 [two_eq_succ_one]
+-- rw [add_succ]
+-- rw [add_succ]
+-- rw [add_zero]
+  rfl
